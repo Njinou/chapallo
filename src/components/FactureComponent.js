45554367eb019485ef4481773adb8cd1e85b9590
Pascal Nitcheu <nitcheupascal@gmail.com>
2021-05-09 23:36:55 +0100

    setting a command

--- src/components/FactureComponent.js
+++ src/components/FactureComponent.js
@@ -45,9 +45,11 @@ const FactureComponent = (props) =>{
     const [comptable, setcomptable] = useState(0);
     const [category, setCategory] = useState("Nouvelle Commande...");
     const [sousCategory, setSousCategory] = useState(null);
-    const [command,setCommand] = useState({});
+    const [command,setCommand] = useState([]);
+    const [article,setArticle] = useState({});
     const [commandTitle,setCommandTitle] = useState("Nouvelle Commande...");
     const [toggleCheckBox, setToggleCheckBox] = useState(false)
+    const [quantite,setQuantite] = useState(0);
 
     const [tab, setTab] = useState([
       {   
@@ -214,24 +216,19 @@ console.log('entrees',Object.keys(Keys['entrees']));*/
 
 
 */
-console.log('NITCHEU NE GE,...');
+
+// command =
 
     return (
         <>
-        <CheckBox
-    disabled={false}
-    value={toggleCheckBox}
-    onValueChange={(newValue) => setToggleCheckBox(newValue)}
-  />
-
-        <FlatList
-        data={tab}
-        refreshing={true}
-        extraData={refreshing}
-        onRefresh={()=>console.log('refreshing...')}
-        renderItem={({ item }) => <CommandLists comptableFunc={comptableFunc} refresh={refreshingFunc} comptable={comptable} item={item} tab={tab} isChecked={isChecked}  func={propSetChecked} funcIsCheck={funcIsCheck}  funcSetTab={funcSetTab} />}
-        keyExtractor={item => item.id +""}
-      />
+          <FlatList
+            data={tab}
+            refreshing={true}
+            extraData={refreshing}
+            onRefresh={()=>console.log('refreshing...')}
+            renderItem={({ item }) => <CommandLists comptableFunc={comptableFunc} refresh={refreshingFunc} comptable={comptable} item={item} tab={tab} isChecked={isChecked}  func={propSetChecked} funcIsCheck={funcIsCheck}  funcSetTab={funcSetTab} />}
+            keyExtractor={item => item.id +""}
+          />
       <View style={{justifyContent:'center',marginLeft:25,marginRight:25,marginTop:20,flexDirection:'row'}}>
                 <TextInput style={{backgroundColor:'white',alignSelf:'stretch'}} onTextChange={()=>console.log('Hello world')} placeholder='Ajouter une Commande'/>
                 <Pressable style={{alignSelf:'center',backgroundColor:'#A9A9A9',marginLeft:5,paddingRight:5}} onPress={()=> setModalVisible(!modalVisible)}>
@@ -240,7 +237,7 @@ console.log('NITCHEU NE GE,...');
                     </Text>
                 </Pressable>
         </View>
-          <View style={{flex:1,margin:25,backgroundColor:'#A9A9A9',borderTopLeftRadius:8,borderTopRightRadius:8,borderBottomLeftRadius:8,borderBottomRightRadius:8}}>
+          <View style={{flex:1,margin:1,backgroundColor:'#A9A9A9',borderTopLeftRadius:8,borderTopRightRadius:8,borderBottomLeftRadius:8,borderBottomRightRadius:8}}>
             <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
               <Text style={{textDecorationLine: 'none', textDecorationStyle: 'solid',fontWeight:'bold',fontSize:20,color:'white'}}>
                   Solid line-through 5
@@ -256,7 +253,7 @@ console.log('NITCHEU NE GE,...');
               }}
             >
              
-              <View style={styles.centeredView}>
+              <View style={styles.centeredView,{width:'90%',alignSelf:'center',paddingTop:'10%',marginBottom:'15%',}}>
                
                <View style={styles.modalView}>
                  <>
@@ -264,20 +261,73 @@ console.log('NITCHEU NE GE,...');
                   <FlatList
                       data={ !sousCategory && (Keys[category] && Object.keys(Keys[category])  ||  (Keys && Object.keys(Keys)))  ||  (sousCategory && Keys[category][sousCategory]) || [] }
                       renderItem={({item}) => 
-                          <View key={item?.code}> 
-                            <TouchableHighlight
-                              style={{ ...styles.openButton, backgroundColor: Keys[category]? 'black': "#A9A9A9" ,margin:5}}
-                              onPress={() => {
-                                Keys[category]? setSousCategory(item): setCategory(item);
-                                console.log('sous category',sousCategory);
-                                console.log('AVANT SOUS  category',category);
-                                sousCategory && console.log(Keys[category][sousCategory]);
-                                //Keys[category]? setCategory(item) : setSousCategory(item);
-                              //  Keys[category][sousCategory] && console.log(Keys[category][sousCategory] );
-                              }}
-                            >
-                              <Text style={styles.textStyle,{padding:2,color:'white',fontSize:22,textAlign:'center'}}>{typeof(item) === 'string'? item : item?.code}</Text>
-                            </TouchableHighlight>
+                          <View key={item?.code} >  
+
+              {  (sousCategory && Keys[category][sousCategory]) ?
+                <View style={{flex:1,flexDirection:'row',padding:1,justifyContent:'center',alignItems:'center',alignSelf:'center',margin:5,}}> 
+                  <CheckBox
+                    disabled={false}
+                    value={toggleCheckBox}
+                    onValueChange={(newValue) => {
+                                  setToggleCheckBox(newValue);
+                                  console.log('newValue item ',item);
+                                  newValue? setQuantite(1) : setQuantite(0);
+                                }
+                              }
+              />
+                  <View style={{ borderTopLeftRadius:8,borderTopRightRadius:1,borderBottomRightRadius:5,borderBottomLeftRadius:1,backgroundColor: "#1E90FF" ,justifyContent:'flex-start',alignItems:'center',width:'50%',marginLeft:5}}> 
+                      <Text  style={styles.textStyle,{padding:5,color:'white',fontSize:18,textAlign:'center',textAlign:'center',textAlignVertical:'top',overflow: 'scroll'}}>{typeof(item) === 'string'? item : item?.description}</Text>
+                  </View>
+
+
+
+                  <View style={{flexDirection:'row',paddingRight:1,justifyContent:'flex-end',marginLeft:10}}>
+                      {quantite >=1 ?<View style={{backgroundColor:'#AAA9A9',alignItems:'center',justifyContent:'center',borderTopLeftRadius:5,borderBottomLeftRadius:1,paddingLeft:'3%',paddingRight:'3%'}}>
+                          <Text style={{color:'gray',fontSize:25}} onPress={() => {
+                              quantite <= 1? setToggleCheckBox(false) : setToggleCheckBox(true);
+                              setQuantite(quantite - 1);
+                              
+                            }
+                            }>-</Text>
+                      </View>:null}
+                      <View style={{backgroundColor:'#F0FFFF',alignItems:'center',justifyContent:'center',paddingLeft:'1%',paddingRight:'1%'}}>
+                          <Text style={{fontWeight:'bold',color:'#AAA9A9'}}>
+                            {quantite}
+                          </Text>
+                      </View>
+                      <View style={{backgroundColor:'#AAA9A9',alignItems:'center',justifyContent:'center',borderTopRightRadius:1,borderBottomRightRadius:5,paddingLeft:'2%',paddingRight:'2%'}}>
+                          <Text style={{color:'gray',fontSize:25}}  onPress={() => {
+                              setQuantite(quantite+1);
+                              setToggleCheckBox(true);
+                            }
+                            }>+</Text>
+                      </View>
+                        
+                        <View style={{justifyContent:'center',marginRight:'auto',marginLeft:'auto'}}>
+                        <Text > { quantite * Number (item?.prix)}</Text>
+                        </View>
+                      
+                    </View>
+
+
+                </View> : 
+                <TouchableHighlight
+                style={{ ...styles.openButton, backgroundColor: Keys[category]  && 'black' || "#A9A9A9" ,margin:5}}
+                onPress={() => {
+                  Keys[category]? setSousCategory(item): setCategory(item);
+                  console.log('sous category',sousCategory);
+                  console.log('AVANT SOUS  category',category);
+                  sousCategory && console.log(Keys[category][sousCategory]);
+                  //Keys[category]? setCategory(item) : setSousCategory(item);
+                //  Keys[category][sousCategory] && console.log(Keys[category][sousCategory] );
+                }}
+              >
+                <Text style={styles.textStyle,{padding:2,color:'white',fontSize:22,textAlign:'center'}}>{typeof(item) === 'string'? item : item?.description}</Text>
+              </TouchableHighlight>
+                }
+
+                            
+                           
                           </View> 
                       }
                       keyExtractor={item => typeof(item) === 'string'? item : item?.code}
@@ -311,10 +361,10 @@ const styles= StyleSheet.create({
         marginTop: 22
       },
       modalView: {
-        margin: 20,
+        margin: 1,
         backgroundColor: "white",
         borderRadius: 20,
-        padding: 35,
+        padding: 5,
         alignItems: "center",
         shadowColor: "#000",
         shadowOffset: {
